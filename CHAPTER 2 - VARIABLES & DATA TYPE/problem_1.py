da50edb13536fb5f878886311644269e5dc64f26
404Shihab <shihabbhuiyan500@gmail.com>
2026-06-17 22:01:04 +0600

    Add input handling and arithmetic operations example in problem_1.py

---
+++ CHAPTER 2 - VARIABLES & DATA TYPE/problem_1.py
@@ -0,0 +1,16 @@
+# Taking input using input()
+a =input("Enter a number: ")
+print(type(a))  # input() always returns a string
+
+# Converting the input to an integer
+b=int(input("Enter a number: "))
+print(type(b))
+
+# ----------------------
+
+# Taking two integer inputs
+num1=int(input("Enter a number: "))
+num2= int(input("Enter another number: "))
+
+# Adding the numbers
+print("Summation of num1 and num2:", num1+num2)
