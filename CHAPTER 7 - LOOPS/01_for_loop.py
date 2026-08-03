e51631fff81832b96d72a4cc4c64f7272d5daed0
404Shihab <shihabbhuiyan500@gmail.com>
2026-08-03 18:46:05 +0600

    Add examples of for loops, break, and continue statements in 01_for_loop.py

---
+++ CHAPTER 7 - LOOPS/01_for_loop.py
@@ -0,0 +1,32 @@
+for i in range(5):
+    print(i+1)
+
+name = "Shihab"
+
+for ch in name:
+    print(ch)
+
+l = [12,34,44,45,23]
+
+for item in l:
+    print(item)
+
+print("--------------")
+#break statement
+
+for i in range (3,9):
+    print(i)
+
+    if(i==7):
+        break
+
+print("---------------")
+#continue
+
+for i in range (34,40):
+    if(i==36):
+        continue
+    print(i)
+
+for i in range (4):
+    pass
