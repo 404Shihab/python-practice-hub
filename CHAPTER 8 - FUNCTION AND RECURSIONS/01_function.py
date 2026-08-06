2e8275c9457c7f9b608e04736a66d2df7e4fdc22
404Shihab <shihabbhuiyan500@gmail.com>
2026-08-06 12:02:35 +0600

    Add examples of while loops, functions, and recursion in respective scripts

---
+++ CHAPTER 8 - FUNCTION AND RECURSIONS/01_function.py
@@ -0,0 +1,18 @@
+def add():
+    a=int(input("Enter a number: "))
+    b=int(input("Enter another number: "))
+    print(a+b)
+
+add()
+
+def subtract(a,b):
+    print(a-b)
+
+subtract(23,4)
+
+def birthday():
+    name = input("Whats your name: ")
+    print("Happy Birthday "+name)
+
+birthday()
+
